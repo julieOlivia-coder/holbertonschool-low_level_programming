901bda388a72bb91e8ba44756a23e962cf17608b
root <boxformations@gmail.com>
2025-02-18 09:08:44 +0000

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - fonction puts et insertion phrase caracte√re
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilangual puzzle\n");
+	return (0);
+}
