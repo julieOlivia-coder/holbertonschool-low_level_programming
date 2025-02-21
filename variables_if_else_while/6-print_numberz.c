d6898f172cac9fc1f4812de459df0b2221e6848d
root <boxformations@]gmail.com>
2025-02-21 13:16:15 +0000

    ASCCI putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - assign the random each time executed
+* betty style doc for function main goes there
+* Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
