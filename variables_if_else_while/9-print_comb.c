646404f7e709875737d72e43222480dee58d0340
root <boxformations@]gmail.com>
2025-02-21 14:58:30 +0000

    finish

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - assign the random each time executed
+* betty style doc for function main goes there
+* Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		putchar('0' + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
