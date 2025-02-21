abc99e8a6f2d2f818739dfc8dcf52fc264f62d26
root <boxformations@]gmail.com>
2025-02-21 10:22:23 +0000

    0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - assign the random each time executed
+* betty style doc for function main goes there
+* Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		printf("%d", i);
+	}
+
+
+	return (0);
+}
