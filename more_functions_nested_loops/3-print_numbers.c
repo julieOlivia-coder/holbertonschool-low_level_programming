748febd5a68c51a899732b92c208aa1720ec86aa
root <boxformations@gmail.com>
2025-02-24 13:05:45 +0000

    prints numbers 0 a 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers - function that prints the numbers, from 0 to 9
+* followed by a new line
+* Return: void
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+
+	_putchar('\n');
+
+}
