7aefe395de3034ffadc0266bec831a1955af35e3
root <boxformations@gmail.com>
2025-02-25 11:03:40 +0000

    %10

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* more_numbers - function that prints the numbers, de 1 to 14
+* followed by a new line
+* Return: void
+*/
+
+void more_numbers(void)
+{
+	int num;
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar(num / 10 + '0');
+			}
+			 _putchar(num % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
