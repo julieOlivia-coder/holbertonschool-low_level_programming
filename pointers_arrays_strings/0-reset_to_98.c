a0236c4844c132260d07728e784b27240cc3c117
root <boxformations@gmail.com>
2025-02-26 10:29:38 +0000

    98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that takes a pointer
+ * @n: nombre pointé
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
