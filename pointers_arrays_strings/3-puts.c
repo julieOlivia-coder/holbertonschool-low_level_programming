18aac139002d14643e64c6809334d72ad8acb2c1
root <boxformations@gmail.com>
2025-02-27 21:18:46 +0000

    sauvegarde

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts(char *str) - function that prints a string
+ * @*str : string pointé
+ * Return: void
+ */
+void _puts(char *str);
+
+{
+		for (; *str != '\0'; str++)
+		_putchar str;
+}
